04ad25f3699b775248edaba0ee1aabc3cd0394e5
maxingmin <ma_xingmin@163.com>
2024-03-20 05:52:10 +0000

    ✨ feat(tourinfo): tourinfo

--- components/TourInfo.js
+++ components/TourInfo.js
@@ -1,9 +1,24 @@
 const TourInfo = ({ tour }) => {
 
+    const { title, description, stops } = tour;
+
     return (
-        <p>
-            "beautify"
-        </p>
+
+        <div className="max-w-2l">
+            <h1 className="text-4xl font-semibold mb-4">{title}</h1>
+            <p className="leading-loose mb-6">{description}</p>
+            <ul>
+                {stops.map((stop) => {
+                    return (
+                        <li key={stop} className="mb-4 p-4 bg-base-100 rounded-xl">
+                            <p className="text">
+                                {stop}
+                            </p>
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
     );
 }
 
